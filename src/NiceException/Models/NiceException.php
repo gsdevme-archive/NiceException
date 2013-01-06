d7337c275be7904182629bdbf7a9110d51ab6ce1
Gavin Staniforth <gavshouse90@gmail.com>
2013-01-06 02:12:30 +0000

    Added exceptionId, nanespace and lines

--- src/NiceException/Models/NiceException.php
+++ src/NiceException/Models/NiceException.php
@@ -8,21 +8,37 @@
 	class NiceException implements JsonSerializable
 	{
 
+		private $_exceptionId;
 		private $_class;
+		private $_namespace;
 		private $_message;
 		private $_line;
+		private $_lines;
 		private $_file;
 
+		public function __construct()
+		{
+			$this->_exceptionId = hash('crc32', uniqid());
+		}
+
 		//	Getters
+		public function getExceptionId(){return $this->_exceptionId;}
 		public function getClass()		{return $this->_class;}
+		public function getNamespace()	{return $this->_namespace;}
 		public function getMessage()	{return $this->_message;}
 		public function getLine()		{return $this->_line;}
+		public function getLines()		{return $this->_lines;}
 		public function getFile()		{return $this->_file;}
 
 		//	Setters
 		public function setClass($class)
 		{
 			$this->_class = $class;
+		}
+
+		public function setNamespace($namespace)
+		{
+			$this->_namespace = $namespace;
 		}
 
 		public function setMessage($message)
@@ -35,6 +51,11 @@
 			$this->_line = (int)$line;
 		}
 
+		public function setLines($lines)
+		{
+			$this->_lines = $lines;
+		}
+
 		public function setFile(SplFileObject $file)
 		{
 			$this->_file = $file;
@@ -43,9 +64,12 @@
 		public function jsonSerialize()
 		{
 			return (object)array(
+				'exceptionId' => $this->getExceptionId(),
 				'class' => $this->getClass(),
+				'namespace' => $this->getNamespace(),
 				'message' => $this->getMessage(),
 				'line' => $this->getLine(),
+				'lines' => $this->getLines(),
 				'file' => $this->getFile()
 			);
 		}
